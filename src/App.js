cc3e202b043ba53625134f256c8201404a916b06
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-11-09 23:12:23 +0100

    step5 logic done

--- src/App.js
+++ src/App.js
@@ -13,18 +13,15 @@ class App extends Component {
     {id: 'b7fcfc20-400f-11ec-ac3d-5f326bb2dcb7', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'ba9f1e40-400f-11ec-ac3d-5f326bb2dcb7', name: 'Eden Clements', number: '645-17-79'},
     {id: 'c907a010-400f-11ec-ac3d-5f326bb2dcb7', name: 'Annie Copeland', number: '227-91-26'},],
-    /* name: '',
-    number: '', */
     filter: '',
   }
-  
-  /* handleChange = (e) => {
-    this.setState({ [e.currentTarget.name]: e.target.value })
-  } */
 
-  onHandlerSubmit = contact => {
+  onHandlerSubmit = data => {
+    if (this.state.contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase())) {
+      return alert('This contact already in the addressbook')
+    }
     this.setState((prevState) => ({
-        contacts: [...prevState.contacts, { id: uuidv1(), name: contact.name, number: contact.number }]
+        contacts: [...prevState.contacts, { id: uuidv1(), name: data.name, number: data.number }]
         }))
   }
 
@@ -32,10 +29,12 @@ class App extends Component {
     this.setState({filter: e.currentTarget.value})
   }
 
-  getFilteredContacts = () => {
+  filterContacts = () => {
     const filteredContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()))
     return filteredContacts;
   }
+
+  
 
   render() {
     return (
@@ -43,14 +42,14 @@ class App extends Component {
         <h1>Phonebook</h1>
         <div className={s.App_content}>
         <Section title="Create a new Contact">
-        <ContactForm /* onHandleChange={this.handleChange} onHandleChangeNumber={ this.handleChange}  */onSubmit={this.onHandlerSubmit} />
+        <ContactForm onSubmit={this.onHandlerSubmit} />
         </Section>
        
         <Section title="Contacts">
             <Filter onHandleFilter={this.handleFilter} />
-            {this.getFilteredContacts().length === 0
+            {this.filterContacts().length === 0
                 ? console.log('ooops')
-                :<ContactList contacts={ this.state.filter ? this.getFilteredContacts() : this.state.contacts}/>
+                :<ContactList contacts={ this.state.filter ? this.filterContacts() : this.state.contacts}/>
             }
           </Section>
         </div>
