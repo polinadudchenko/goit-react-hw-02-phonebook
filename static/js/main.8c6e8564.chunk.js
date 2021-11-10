8e737655b4c7b0fc17e50a2ca5db00916080b8cc
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-11-10 01:20:11 +0100

    Updates

--- static/js/main.8c6e8564.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));var a=function(){return Object(s.jsx)("div",{className:"App"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.8c6e8564.chunk.js.map
