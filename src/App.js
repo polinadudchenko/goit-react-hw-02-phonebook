459319f185436a5899c16cd959452bccd8ea558e
Polina Dudchenko <pv.dudchenko@gmail.com>
2021-11-10 13:29:15 +0100

    names in contacts updated

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import Section from './components/Section'
 class App extends Component {
   
   state = {
-  contacts: [{id: 'ba9f1e30-400f-11ec-ac3d-5f326bb2dcb7', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'b7fcfc20-400f-11ec-ac3d-5f326bb2dcb7', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'ba9f1e40-400f-11ec-ac3d-5f326bb2dcb7', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'c907a010-400f-11ec-ac3d-5f326bb2dcb7', name: 'Annie Copeland', number: '227-91-26'},],
+  contacts: [{id: 'ba9f1e30-400f-11ec-ac3d-5f326bb2dcb7', name: 'Harry Potter', number: '459-12-56'},
+    {id: 'b7fcfc20-400f-11ec-ac3d-5f326bb2dcb7', name: 'Hermine Granger', number: '443-89-12'},
+    {id: 'ba9f1e40-400f-11ec-ac3d-5f326bb2dcb7', name: 'Ron Weasley', number: '645-17-79'},
+    {id: 'c907a010-400f-11ec-ac3d-5f326bb2dcb7', name: 'Albus Dumbledore', number: '227-91-26'},],
     filter: '',
   }
 
